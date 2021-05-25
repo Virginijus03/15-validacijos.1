b832422a890455e893bb347c7b4f23f9bc29861c
Virginijus03 <klimauskas@yahoo.com>
2021-05-25 18:01:59 +0300

    nebaigtas

--- js/sum/sum.test.js
+++ js/sum/sum.test.js
@@ -13,5 +13,14 @@ test('adds 0.1 + 0.2 to equal 0.3', () => {
     expect(sum(0.1, 0.2)).toBeCloseTo(0.3, 5);
 });
 
+test('string is not allowed', () => {
+    expect(sum(NaN, 'a')).toBeFalsy();
+});
+
+test('adds 0.5 + 0.5 to equal 1', () => {
+    expect(sum(0.6, 0.5)).toBeCloseTo(1.1, 5); S
+});
+
+
 
 
